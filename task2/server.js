44fd6b556689f8b82e2b205430fc28c699bff920
Olufemi Adewale <olufemiadewale@192.168.1.110>
2025-01-03 12:39:28 +0300

    added tasks files

---
+++ task2/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const os = require('os');
+
+const hostname = '0.0.0.0';
+const port = 8080;
+const serverName = os.hostname();
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Hello, World!\nHandled by server: ${serverName}`);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running on port ${port} (Handled by: ${serverName})`);
+});
